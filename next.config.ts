2db485f29ebad58965d80c1e37cbb09b9ffba127
andretakeo <andrefujiwara1@gmail.com>
2025-12-19 17:48:55 -0300

    Update configuration and styles: Add remote image patterns, new dependencies, and enhance global styles with custom properties and animations.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "shared.akamai.steamstatic.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
