338f94e7166c700dc924ead357b76b591c81688f
Lucca Costa Lima D'Avila <luccacldavila@gmail.com>
2026-03-16 16:11:31 -0300

    Refazendo as funções

---
+++ gitguts/src/Componentes/TelaInicial.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function TelaInicial() {
+    
+    return (
+
+        <div>
+            <title>GitGuts - Tela Inicial</title>
+            <h1>Bem-vindo ao GitGuts!</h1>
+            <p>Gitgut é um site de jogos</p>
+            <script>
+                const imagens = []
+            </script>
+            <footer>
+                <p>© 2024 GitGuts. Todos os direitos reservados.</p>
+                <p>Desenvolvido por Equipe GitGuts.</p>
+                <p>email@exemplo.com</p>
+                <p>Telefone: (123) 456-7890</p>
+            </footer>
+            
+            
+        </div>
+    );
+}
